2f7dfabbe410c2ba63c5b7b082b375fb36f72790
Louis Maddox <louismmx@gmail.com>
2021-09-03 11:11:46 +0100

    Document lineterm support tests

--- tests/lineterm_support_test.py
+++ tests/lineterm_support_test.py
@@ -10,6 +10,10 @@ from pytest import mark
     [],
 )])
 def test_no_lineterm_support(sample_str, sep, linesep, expected):
+    """
+    Show that the csv module can't read a CSV with non-standard lineterminator (a tilde)
+    because the enumerated rows of the reader is an empty list, i.e. nothing is read.
+    """
     r = csv.DictReader(io.StringIO(sample_str), delimiter=sep, lineterminator=linesep)
     assert list(r) == expected
 
@@ -23,6 +27,10 @@ def test_no_lineterm_support(sample_str, sep, linesep, expected):
     ],
 )])
 def test_implicit_lineterm_support(sample_str, sep, expected):
+    """
+    Show that the csv module can read a CSV with the newline char `\n` without it being
+    specified (implicitly "\n" or "\r" are used).
+    """
     r = csv.DictReader(io.StringIO(sample_str), delimiter=sep)
     assert list(r) == expected
 
@@ -33,5 +41,8 @@ def test_implicit_lineterm_support(sample_str, sep, expected):
     list(range(1,10)),
 )])
 def test_implicit_lineterm_support(sample_str, sep, linesep, expected):
+    """
+    Show that pandas can read a CSV with the tilde char.
+    """
     df = pd.read_csv(io.StringIO(sample_str), sep=sep, lineterminator=linesep)
     assert df.values.ravel().tolist() == expected
